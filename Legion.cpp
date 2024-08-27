159378e3592055da663fe2bb33b746c7f7ce45d2
Saskia Steyn <u17267162@tuks.co.za>
2024-08-27 08:18:43 +0200

    updated legion and added more tests (100% for testing)

--- Legion.cpp
+++ Legion.cpp
@@ -6,10 +6,26 @@ Legion::Legion() {
 
 void Legion::move() {
     cout << this->getUnitName() << " legion is moving." << endl;
+    cout << "Executing order MOVE for all sub-legions: " << endl;
+
+    for (int i = 0; i < this->unitComponent.size(); i++) {
+        if (this->unitComponent[i] != nullptr) {
+            this->unitComponent[i]->move();
+        }
+    }
+
 }
 
 void Legion::attack() {
     cout << this->getUnitName() << " is attacking." << endl;
+
+    cout << "Executing order ATTACK for all sub-legions: " << endl;
+
+    for (int i = 0; i < this->unitComponent.size(); i++) {
+        if (this->unitComponent[i] != nullptr) {
+            this->unitComponent[i]->attack();
+        }
+    }
 }
 
 bool Legion::add(UnitComponent *component) {
